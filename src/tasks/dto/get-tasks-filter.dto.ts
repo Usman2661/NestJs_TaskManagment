3279d0a52051d0c799e1e94b57ae89915dcbca6b
Usman Ali <uali@modern-networks.co.uk>
2020-05-14 15:39:45 +0100

    create task/get task by ID using pgadmin typeorm

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,5 +1,5 @@
-import { TaskStatus } from '../task.model';
 import { IsNotEmpty, IsOptional, IsIn } from 'class-validator';
+import { TaskStatus } from '../task-status.enum';
 export class GetTasksFilterDto {
   @IsOptional()
   @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
