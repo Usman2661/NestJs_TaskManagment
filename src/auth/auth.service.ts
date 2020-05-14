d58ca7315f139bb5c151684a6f1fbb63e3f87875
Usman Ali <uali@modern-networks.co.uk>
2020-05-14 17:26:22 +0100

    sign in and signup functionality added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.respository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -8,4 +9,16 @@ export class AuthService {
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
   ) {}
+
+  async signup(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.signUp(authCredentialsDto);
+  }
+
+  async sigin(authCredentialsDto: AuthCredentialsDto) {
+    const username = await this.userRepository.validateUser(authCredentialsDto);
+
+    if (!username) {
+      throw new UnauthorizedException('Invalid Credentials');
+    }
+  }
 }
