6eccf53be4dcd9eb0a1133e68c10645f48335e79
Usman Ali <uali@modern-networks.co.uk>
2020-05-19 17:11:29 +0100

    tests for repository and entity

---
+++ src/auth/user.entity.spec.ts
@@ -0,0 +1,24 @@
+import * as bcrypt from 'bcryptjs';
+import { User } from './user.entity';
+
+describe('User Entity', () => {
+  let user: User;
+
+  beforeEach(() => {
+    user = new User();
+    user.password = 'testPassword';
+    user.salt = 'testSalt';
+    bcrypt.hash = jest.fn();
+  });
+  describe('validatePassword', () => {
+    it('returns true as password is valid', async () => {
+      bcrypt.hash.mockReturnValue('testPassword');
+      expect(bcrypt.hash).not.toHaveBeenCalled();
+      const result = await user.validatePassword('54154');
+      expect(bcrypt.hash).toHaveBeenCalledWith('54154', 'testSalt');
+
+      expect(result).toBeTruthy();
+    });
+    it('returns false as password is invalid', () => {});
+  });
+});
--- src/auth/user.repository.spec.ts
+++ src/auth/user.repository.spec.ts
@@ -1,6 +1,11 @@
 import { Test } from '@nestjs/testing';
 import { UserRepository } from './user.respository';
-import { ConflictException } from '@nestjs/common';
+import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 const mockCredentialsDto = {
   username: 'TestUser',
@@ -35,6 +40,61 @@ describe('UserRepository', () => {
       expect(userRepository.signUp(mockCredentialsDto)).rejects.toThrow(
         ConflictException,
       );
+    });
+
+    it('thows an unhandled error', () => {
+      save.mockRejectedValue({ code: '4545' }); // Unhandled Error Code
+
+      expect(userRepository.signUp(mockCredentialsDto)).rejects.toThrow(
+        InternalServerErrorException,
+      );
+    });
+  });
+
+  describe('validateUserPassword', () => {
+    let user;
+
+    beforeEach(() => {
+      userRepository.findOne = jest.fn();
+      user = new User();
+      user.username = 'TestUsername';
+      user.validatePassword = jest.fn();
+    });
+    it('returns the username as validation is succesfull', async () => {
+      userRepository.findOne.mockResolvedValue(user);
+      user.validatePassword.mockResolvedValue(true);
+
+      const result = await userRepository.validateUser(mockCredentialsDto);
+      expect(result).toEqual('TestUsername');
+    });
+    it('returns null as the user cannot be found', async () => {
+      userRepository.findOne.mockResolvedValue(null);
+      const result = await userRepository.validateUser(mockCredentialsDto);
+      expect(user.validatePassword).not.toHaveBeenCalled();
+      expect(result).toBeNull();
+    });
+
+    it('returns null when the password is invalid', async () => {
+      userRepository.findOne.mockResolvedValue(user);
+      user.validatePassword.mockResolvedValue(false);
+      const result = await userRepository.validateUser(mockCredentialsDto);
+      expect(user.validatePassword).toHaveBeenCalled();
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('hasPassword', () => {
+    it('call brypt.hash to generate the hash', async () => {
+      bcrypt.hash = jest.fn().mockResolvedValue('testHash');
+      expect(bcrypt.hash).not.toHaveBeenCalled();
+
+      const result = await userRepository.hashPassword(
+        'testPassword',
+        'testSalt',
+      );
+      expect(bcrypt.hash).toHaveBeenCalledWith('testPassword', 'testSalt');
+
+      expect(result).toEqual('testHash');
     });
   });
 });
